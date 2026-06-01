d14c890ff608ce7187a16281f79220f6e3d6a1c5
AlarkiusEJ <84819182+AlarkiusJay@users.noreply.github.com>
2026-06-01 03:53:41 -0700

    feat: add /bn-naiseikai-universe redirect page with 5s countdown

--- alarkiusej-site/src/App.tsx
+++ alarkiusej-site/src/App.tsx
@@ -12,6 +12,7 @@ import Contact from './pages/Contact'
 import Tools from './pages/Tools'
 import FindMyBooks from './pages/FindMyBooks'
 import BnHibrythianSaga from './pages/BnHibrythianSaga'
+import BnNaiseikaiUniverse from './pages/BnNaiseikaiUniverse'
 
 export default function App() {
   return (
@@ -31,6 +32,7 @@ export default function App() {
             <Route path="/tools" element={<Tools />} />
             <Route path="/find-my-books" element={<FindMyBooks />} />
             <Route path="/bn-hibrythian-saga" element={<BnHibrythianSaga />} />
+            <Route path="/bn-naiseikai-universe" element={<BnNaiseikaiUniverse />} />
           </Routes>
         </main>
         <Footer />
