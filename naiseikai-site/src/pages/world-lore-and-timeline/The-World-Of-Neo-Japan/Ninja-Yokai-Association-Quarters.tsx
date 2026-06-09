ddb8633e1df22e3579021d28e52851264882295d
AlarkiusEJ <84819182+AlarkiusJay@users.noreply.github.com>
2026-06-09 11:45:50 +0000

    fix(Ninja-Yokai-Association-Quarters): correct LorePageLayout import path ../../../

--- naiseikai-site/src/pages/world-lore-and-timeline/The-World-Of-Neo-Japan/Ninja-Yokai-Association-Quarters.tsx
+++ naiseikai-site/src/pages/world-lore-and-timeline/The-World-Of-Neo-Japan/Ninja-Yokai-Association-Quarters.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import LorePageLayout from '../../../../components/LorePageLayout'
+import LorePageLayout from '../../../components/LorePageLayout'
 
 const ACCENT = '#7ef5ff';
 const GOLD = '#c8a87a';
