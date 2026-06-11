c45a094780e0b77cb974d371ec49e05cc1766788
AlarkiusEJ <84819182+AlarkiusJay@users.noreply.github.com>
2026-06-11 08:09:21 +0000

    feat: add /32-16TimeSystem route to hibrythia-site

--- hibrythia-site/src/App.tsx
+++ hibrythia-site/src/App.tsx
@@ -5,6 +5,7 @@ import LegendsAndMyths from '@/pages/LegendsAndMyths';
 import OurWorldbuilding from '@/pages/OurWorldbuilding';
 import OurCharacters from '@/pages/OurCharacters';
 import Bookshelf from '@/pages/Bookshelf';
+import TimeSystem from '@/pages/TimeSystem';
 import NotFound from '@/pages/NotFound';
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="world" element={<OurWorldbuilding />} />
         <Route path="characters" element={<OurCharacters />} />
         <Route path="bookshelf" element={<Bookshelf />} />
+        <Route path="32-16TimeSystem" element={<TimeSystem />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
